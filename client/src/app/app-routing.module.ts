62e617e1c944c7cb65966ca635dd98b16b1979cc
Vanina Riba <vanina.riba@gmail.com>
2021-02-28 13:04:59 -0300

    client create routes, component and css

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {IncomeComponent} from './income/income.component';
+import {ExpenseComponent} from './expense/expense.component';
+import { HomeComponent } from "./home/home.component";
+import { AddComponent } from "./add/add.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'income', component: IncomeComponent},
+  {path: 'expense', component: ExpenseComponent},
+  {path: 'add', component: AddComponent},
+
+    ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { IncomeComponent } from './income/income.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    IncomeComponent,
+    ExpenseComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
